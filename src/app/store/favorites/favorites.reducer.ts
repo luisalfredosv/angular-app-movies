7823fbbc3c65fe660490e6a3327b111e2ac50392
Luis Alfredo SV <luissarabia80@gmail.com>
2020-12-20 20:40:16 -0400

    confirmado

--- src/app/store/favorites/favorites.reducer.ts
+++ src/app/store/favorites/favorites.reducer.ts
@@ -2,21 +2,14 @@ import { MoviesFavoritas } from './favorites.model';
 import * as MoviesFavoritasActions from './favorites.actions'
 
 
-const initialState: MoviesFavoritas = {
-  id: 0,
-  name: '',
-  imgUrl: '',
-  date:null
-}
+// const initialState: MoviesFavoritas[] = [];
 
-
-export function reducer( state: MoviesFavoritas[] = [initialState],  action: MoviesFavoritasActions.Actions){
+export function reducer( state: MoviesFavoritas[] = [],  action: MoviesFavoritasActions.Actions){
     switch (action.type){
       case MoviesFavoritasActions.ADD_MOVIE_FAV:
         return [...state, action.payload];
       case MoviesFavoritasActions.DEL_MOVIE_FAV:
-        
-        // state.splice(action.payload, 1);
+        state.splice(action.payload, 1);
         return state;
       default:
         return state;
